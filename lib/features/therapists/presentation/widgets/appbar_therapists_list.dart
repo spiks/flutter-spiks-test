4dbd19f02adb47b2c13db262b01327898b1397cd
Alexander Panin <ale21111681@yandex.ru>
2023-09-30 23:54:49 +0300

    init test app

---
+++ lib/features/therapists/presentation/widgets/appbar_therapists_list.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_spiks_test/core/values/icons/outline_icons.dart';
+import 'package:flutter_spiks_test/generated/l10n.dart';
+
+class AppbarTherapists extends StatelessWidget implements PreferredSizeWidget {
+  const AppbarTherapists({super.key});
+
+  @override
+  Size get preferredSize => const Size.fromHeight(60);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
+
